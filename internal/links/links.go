e32f78e3721ca5ae1270e0750e3aaf146a8730d9
Mariana Victor <marianavictorf@gmail.com>
2023-11-13 14:27:07 -0300

    Links update

--- internal/links/links.go
+++ internal/links/links.go
@@ -38,3 +38,29 @@ func (link Link) Save() int64 {
 	return id
 
 }
+
+func GetAll() []Link {
+	stmt, err := database.Db.Prepare("SELECT id, title, address FROM Links")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	rows, err := stmt.Query()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	var links []Link
+	for rows.Next() {
+		var link Link
+		err := rows.Scan(&link.ID, &link.Title, &link.Address)
+		if err != nil {
+			log.Fatal(err)
+		}
+		links = append(links, link)
+	}
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return links
+}
