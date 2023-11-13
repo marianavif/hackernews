182ee0d8de328358818d77adf89cfbc293f63406
Mariana Victor <marianavictorf@gmail.com>
2023-11-13 14:12:12 -0300

    createlink mutation

---
+++ internal/links/links.go
@@ -0,0 +1,40 @@
+package links
+
+import (
+	"log"
+
+	database "github.com/marianavif/hackernews/internal/pkg/db/mysql"
+	"github.com/marianavif/hackernews/internal/users"
+)
+
+// #1
+
+type Link struct {
+	ID      string
+	Title   string
+	Address string
+	User    *users.User
+}
+
+// #2
+
+func (link Link) Save() int64 {
+	// #3
+	stmt, err := database.Db.Prepare("INSERT INTO Links(Title,Address) VALUES(?,?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// #4
+	res, err := stmt.Exec(link.Title, link.Address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// #5
+	id, err := res.LastInsertId()
+	if err != nil {
+		log.Fatal("Error:", err.Error())
+	}
+	log.Print("Row inserted!")
+	return id
+
+}
